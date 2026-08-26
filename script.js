26e2ab0983aa49b47f5bea0e9b74ead404f8fb83
Akkash Sharma <uvaannssharma@gmail.com>
2026-08-26 15:55:28 +0530

    Update script URL

--- script.js
+++ script.js
@@ -55,7 +55,25 @@ if (leadForm) {
 
             return;
         }
+const scriptURL = "https://script.google.com/macros/s/AKfycbyQMgG-upRvzq_rFoKJnJaSIf00nCkjyl0xtyzv-uY6_erWGjC1Ec1FEwCu2N9slodx/exec";
 
+const formData = new URLSearchParams();
+
+formData.append("name", name);
+formData.append("email", email);
+formData.append("phone", phone);
+console.log("FETCH STARTING", scriptURL);
+fetch(scriptURL, {
+    method: "POST",
+    mode: "no-cors",
+    body: formData
+})
+.then(function () {
+    console.log("Lead sent to Google Sheet");
+})
+.catch(function (error) {
+    console.error("Lead submission error:", error);
+});
 
         // Success message
 
